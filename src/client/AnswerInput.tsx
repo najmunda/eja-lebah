91efac6f388ec2201b999dd97f83b8a4201cb1ba
Najmunda <najmundaakmal@gmail.com>
2025-11-10 14:25:45 +0700

    feat: add caret in top of input
    wrap input, add p and caret (double text)

--- src/client/AnswerInput.tsx
+++ src/client/AnswerInput.tsx
@@ -6,16 +6,21 @@ export default function AnswerInput({ value, ref, handleKeyDown }) {
   const { isLoading } = useContext(AppContext);
 
   return (
-    <input 
-      type="text" 
-      name="" 
-      id="" 
-      ref={ref}
-      value={value}
-      onKeyDown={handleKeyDown}
-      className={clsx("p-2 text-center bg-yellow-200 text-4xl rounded-lg", {
+    <div
+      className={clsx("flex justify-center items-center bg-yellow-200 text-4xl rounded-lg relative", {
         "animate-pulse": isLoading,
       })}
-    />
+    >
+      <input 
+        type="text" 
+        name="" 
+        id="" 
+        ref={ref}
+        value={value}
+        onKeyDown={handleKeyDown}
+        className="p-2 h-full w-full text-center text-yellow-200 caret-yellow-200 rounded-lg"
+      />
+      <p className="absolute">{value}<span className="animate-fade-repeat">|</span></p>
+    </div>
   )
 }
