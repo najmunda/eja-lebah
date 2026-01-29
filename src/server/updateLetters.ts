173d4de96f1ea6e222c11a63dcdcb8f460345e95
Najmunda <najmundaakmal@gmail.com>
2026-01-29 14:58:44 +0700

    feat: limit get letter loop; unite 2 selectWord; random vocal 2/3

--- src/server/updateLetters.ts
+++ src/server/updateLetters.ts
@@ -1,25 +1,14 @@
-import { countWordScore } from "../utils.js";
+import { countWordScore, getRandomInt } from "../utils.js";
 import db from "./db.js";
 
-const whereClause = `
+const selectWordsStatement = db.prepare(`
+  SELECT DISTINCT word
+  FROM Word
   WHERE 
     length(word) >= 4 -- Answer min length
     AND regexp('^[a-z]+$', word) = 1 -- Only contains alphabet
     AND instr(word, ?) > 0 -- Must contain key letter
     AND regexp(?, word) = 1
-`;
-
-const getWordCountStatement = db.prepare(`
-  SELECT COUNT(DISTINCT word) AS word_count
-  FROM Word 
-  ${whereClause}
-  ;
-`);
-
-const getWordsStatement = db.prepare(`
-  SELECT DISTINCT word
-  FROM Word 
-  ${whereClause}
   ;
 `);
 
@@ -27,9 +16,18 @@ const insertLettersStatement = db.prepare(`
   INSERT INTO Letter (key_letter, letters, words, word_count, max_score) VALUES (?, ?, ?, ?, ?);
 `);
 
+const selectLastLetterIdStatement = db.prepare(
+  `SELECT seq FROM sqlite_sequence WHERE name = 'Letter';`,
+);
+
+const insertPrevLetterStatement = db.prepare(`
+  INSERT INTO Letter (key_letter, letters, words, word_count, max_score)
+  SELECT key_letter, letters, words, word_count, max_score FROM Letter WHERE id = ?;
+`);
+
 function getRandomLetters() {
   const LETTERS_TOTAL = 7;
-  const VOCAL_TOTAL = 2;
+  const VOCAL_TOTAL = getRandomInt(2, 3);
 
   const vocalLetters = "aiueo";
   const consonantLetters = "bcdfghjklmnpqrstvwxyz";
@@ -37,53 +35,61 @@ function getRandomLetters() {
 
   let i = 1;
   while (i <= VOCAL_TOTAL) {
-    const randomIndex = Math.floor(Math.random() * vocalLetters.length);
+    const randomIndex = getRandomInt(0, vocalLetters.length - 1);
     const randomVocal = vocalLetters[randomIndex];
     if (chosenLetters.includes(randomVocal)) continue;
     chosenLetters += randomVocal;
     i++;
   }
   while (i <= LETTERS_TOTAL) {
-    const randomIndex = Math.floor(Math.random() * consonantLetters.length);
+    const randomIndex = getRandomInt(0, consonantLetters.length - 1);
     const randomConsonant = consonantLetters[randomIndex];
     if (chosenLetters.includes(randomConsonant)) continue;
     chosenLetters += randomConsonant;
     i++;
   }
 
-  const randomIndex = Math.floor(Math.random() * LETTERS_TOTAL);
+  const randomIndex = getRandomInt(0, LETTERS_TOTAL - 1);
   const keyLetter = chosenLetters[randomIndex];
   const includeAllLettersRegex = `\\b[${chosenLetters}]+\\b`;
-  chosenLetters = chosenLetters.replace(keyLetter, "");
-  const wordCount = getWordCountStatement.get(
-    keyLetter,
-    includeAllLettersRegex,
-  )["word_count"];
+  const otherLetters = chosenLetters.replace(keyLetter, "");
+  const words = selectWordsStatement
+    .all(keyLetter, includeAllLettersRegex)
+    .map((row: { word: string }) => row.word);
 
-  return [keyLetter, chosenLetters, includeAllLettersRegex, wordCount];
+  return { keyLetter, otherLetters, words };
 }
 
 export default function updateLettersAndAnswers() {
   const ANSWER_TOTAL_MIN = 10;
-  let wordCount = 0;
-  let keyLetter, chosenLetters, includeAllLettersRegex;
-  while (wordCount < ANSWER_TOTAL_MIN) {
-    [keyLetter, chosenLetters, includeAllLettersRegex, wordCount] =
-      getRandomLetters();
+  const LOOP_LIMIT = 100;
+  let words = [];
+  let keyLetter, otherLetters;
+  let loopCount = 0;
+  while (
+    words.length < ANSWER_TOTAL_MIN ||
+    new Set(words.join("")).size !== 7
+  ) {
+    ({ keyLetter, otherLetters, words } = getRandomLetters());
+    loopCount++;
+    if (loopCount >= LOOP_LIMIT) break;
   }
-  const words = getWordsStatement
-    .all(keyLetter, includeAllLettersRegex)
-    .map((row) => row.word);
-  const wordsJSON = JSON.stringify(words);
-  const maxScore = words.reduce(
-    (currScore, word) => currScore + countWordScore(word),
-    0,
-  );
-  insertLettersStatement.run(
-    keyLetter,
-    chosenLetters,
-    wordsJSON,
-    wordCount,
-    maxScore,
-  );
+  if (loopCount < LOOP_LIMIT) {
+    const wordsJSON = JSON.stringify(words);
+    const maxScore = words.reduce(
+      (currScore: number, word: string) => currScore + countWordScore(word),
+      0,
+    );
+    insertLettersStatement.run(
+      keyLetter,
+      otherLetters,
+      wordsJSON,
+      words.length,
+      maxScore,
+    );
+  } else {
+    const { seq: lastLetterId } = selectLastLetterIdStatement.get();
+    const randomId = getRandomInt(1, lastLetterId - 1);
+    insertPrevLetterStatement.run(randomId);
+  }
 }
--- src/utils.ts
+++ src/utils.ts
@@ -3,3 +3,7 @@ export function countWordScore(word: string): number {
   if (new Set(word).size === 7) return word.length + 7;
   return word.length;
 }
+
+export function getRandomInt(min, max): number {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
