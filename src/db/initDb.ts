1bc8317ca07ccf104e7af7c6bfb1f1ccbad5b063
Najmunda <najmundaakmal@gmail.com>
2026-03-26 15:07:15 +0700

    fix: set db init script for deploy with remove regex extension
    make init and seed in one file script

--- src/db/initDb.ts
+++ src/db/initDb.ts
@@ -1,12 +1,11 @@
 import Database from "better-sqlite3";
-import * as sqlite_regex from "sqlite-regex";
 import { readFileSync } from "node:fs";
 import path, { dirname } from "node:path";
 import { fileURLToPath } from "node:url";
 
 const db = new Database(process.env.DB_NAME);
 db.pragma("journal_mode = WAL");
-db.loadExtension(sqlite_regex.getLoadablePath());
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 db.exec(readFileSync(path.resolve(__dirname, "schema.sql"), "utf-8"));
+db.exec(readFileSync(path.resolve(__dirname, "seed.sql"), "utf-8"));
