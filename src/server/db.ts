f664bf22033a5e255be83e2a6674360a860c6b59
Najmunda <najmundaakmal@gmail.com>
2026-03-27 14:46:33 +0700

    fix: change sqlite regex from package to loadable extension

--- src/server/db.ts
+++ src/server/db.ts
@@ -1,10 +1,18 @@
 import Database from "better-sqlite3";
-import * as sqlite_regex from "sqlite-regex";
+import { sql } from "../utils.js";
 
 const db = new Database(process.env.DB_NAME, {
   fileMustExist: true,
 });
 db.pragma("journal_mode = WAL");
-db.loadExtension(sqlite_regex.getLoadablePath());
+db.loadExtension("./regex0");
+console.log(
+  db
+    .prepare(sql`
+      SELECT
+        regex_version ()
+    `)
+    .get(),
+);
 
 export default db;
